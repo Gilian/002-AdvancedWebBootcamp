0445aef520ae7dff990ed3c44beedef0d616d1e3
Tobias Em <gilian@web.de>
2018-05-20 22:28:15 +0200

    Fix second example so it is actually working

--- 07-Introduction-To-AJAX/bitcoin-app/js/api-fetch-2.js
+++ 07-Introduction-To-AJAX/bitcoin-app/js/api-fetch-2.js
@@ -1,11 +1,12 @@
 let url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+var price = document.querySelector('#price');
 
 fetch(url)
     .then(function (response) {
         console.log(response);
-        return response.text();
+        return response.json();
     })
     .then(function (data) {
-        console.log(data)
-        //console.log(typeof data.bpi.EUR.rate);
+        console.log(data);
+        price.innerText = data.bpi.EUR.rate + "€";
     });
